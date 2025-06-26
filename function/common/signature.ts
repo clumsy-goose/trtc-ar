12cf3e738c1f3841b03e26966b69dcf62fcc90a1
corbinlin <corbinlin@tencent.com>
2025-06-26 14:55:35 +0800

    fix edge function

--- function/common/signature.ts
+++ function/common/signature.ts
@@ -1,7 +1,10 @@
-export function onRequestPost({request}: {request: any}) {
+export async function onRequestPost({request}: {request: any}) {
   console.log("🚀 ~ onRequestPost ~ request:", request);
-  // const url = 'https://test-api.trtc.io/api/common/signature';
-  return new Response(request, {
+  const url = 'https://test-api.trtc.io/common/signature';
+  const res = await (await fetch(url, {
+    method: 'POST',
+  })).text();
+  return new Response(res, {
     headers: {
       'content-type': 'application/json; charset=UTF-8',
       'Access-Control-Allow-Origin': '*',
--- lib/api.ts
+++ lib/api.ts
@@ -2,4 +2,4 @@ import { request } from './request';
 
 export const API_DOMAIN = 'test-api.trtc.io';
 export const webArSignature = async () =>
-  process.env.NODE_ENV === 'development' ? request({ method: 'post', url: '/api/common/signature' }) : fetch('https://test-api.trtc.io/common/signature',{method:'post'})
+  process.env.NODE_ENV === 'development' ? request({ method: 'post', url: '/api/common/signature' }) : fetch('/common/signature',{method:'post'})
