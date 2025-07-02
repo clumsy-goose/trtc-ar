a2e3fbd3e92d4021289fce3a3019febb0a636e59
corbinlin <corbinlin@tencent.com>
2025-07-02 12:01:36 +0800

    fix : 修改配置

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
-import type { NextConfig } from "next";
-console.log('env: ',process.env.NODE_ENV,process.env.LICENSE_KEY,process.env.APPID);
+import type { NextConfig } from 'next';
+
 const baseConfig: NextConfig = {
   transpilePackages: ['tdesign-react'],
   images: {
@@ -7,21 +7,19 @@ const baseConfig: NextConfig = {
   },
   webpack: (config, options) => {
     const { isServer } = options;
-    config.module.rules.push(
-      {
-        test: /\.(glb|hdr|mp4|mov|mp3|wav|webm)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next/static/images/',
-              outputPath: `${isServer ? '../' : ''}static/images/`,
-              name: '[name].[hash:8].[ext]',
-            },
+    config.module.rules.push({
+      test: /\.(glb|hdr|mp4|mov|mp3|wav|webm)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/images/',
+            outputPath: `${isServer ? '../' : ''}static/images/`,
+            name: '[name].[hash:8].[ext]',
           },
-        ],
-      },
-    );
+        },
+      ],
+    });
     config.optimization = {
       ...config.optimization,
       splitChunks: {
@@ -32,29 +30,13 @@ const baseConfig: NextConfig = {
         maxInitialRequests: 3,
       },
     };
-    if (!isServer) {
-      // config.plugins.push(new BundleAnalyzerPlugin());
-    }
     return config;
-  },  
-}
-
-const nextConfig: NextConfig = process.env.NODE_ENV === 'production' ? {
-  ...baseConfig,
-  output:'export'
-} : {
-  ...baseConfig,
-  async rewrites(){
-    if(process.env.NODE_ENV === 'production') return [];
-    const rules = [
-      {
-        source: '/api/common/signature',
-        destination: `https://${process.env.API_DOMAIN}/common/signature`,
-      },
-    ];
-    console.log('rulers',rules,'process.env.NODE_ENV',process.env.NODE_ENV)
-    return rules;
   },
-}
+};
+
+const nextConfig: NextConfig = {
+  ...baseConfig,
+  output: 'export',
+};
 
 export default nextConfig;
